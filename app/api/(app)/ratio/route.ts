eff8a33f38ac6c21a02a2020ae4eb5c2e3fe279e
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2024-04-06 17:12:43 +0900

    3つづつ実行

--- app/api/(app)/ratio/route.ts
+++ app/api/(app)/ratio/route.ts
@@ -17,10 +17,12 @@ export async function GET() {
    })())
   }
  }
- await Promise.all(promise)
+ for(let i=0;i<promise.length;i+=3){
+   await Promise.all(promise.splice(0, 3))
+ }
+
  return NextResponse.json({ ok: true, data: result })
 }
-
 
 async function get(eventId: number, timeId: number) {
  return prisma.raffle.count({
